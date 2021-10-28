db08010962004ad94b45cd24895101c3a828a04c
BoilingNoob <morgandowd@teksavvy.com>
2021-10-28 02:14:48 -0400

    created basic project requirments

---
+++ encryption_protocal.java
@@ -0,0 +1,10 @@
+public class encryption_protocal{
+	public static void main(String args[]){
+
+	}
+}
+
+//use a key to encrypt a message to send over sms with a head that can be recognised
+
+// detect a head in an sms and use your key to decrypt it in app.
+//require login in each app open to prevent the screen hijacking exploit.
